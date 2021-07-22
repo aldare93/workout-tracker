e68f36f636a5962176b9a036f9dffa6f710176e3
Alex DaRe <alexdare93@gmail.com>
2021-07-22 01:12:22 -0700

    routes added to api.js

--- routes/api.js
+++ routes/api.js
@@ -1,7 +1,28 @@
 const router = require("express").Router();
 const Workouts = require("../models/workouts.js");
 
-router.get("/api/workouts", (req, res) => {
+router.get("/workouts", (req, res) => {
+    Workouts.find({})
+    .then(dbWorkouts => {
+        res.json(dbWorkouts);
+    })
+    .catch(error => {
+        res.status(400).json(error);
+    });
+});
+
+router.post("/exercise", ({ body }, res) => {
+    Workouts.create(body)
+    .then(dbWorkouts => {
+        res.json(dbWorkouts);
+        res.render("/exercise");
+    })
+    .catch(error => {
+        res.status(400).json(error);
+    });
+});
+
+router.get("/stats", (req, res) => {
     Workouts.find({})
     .then(dbWorkouts => {
         res.json(dbWorkouts);
