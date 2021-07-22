5d99f7aebb64052de6eff611ea9b818ee5e69801
Alex DaRe <alexdare93@gmail.com>
2021-07-22 01:25:15 -0700

    routes.js modified

--- routes/api.js
+++ routes/api.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 const Workouts = require("../models/workouts.js");
 
-router.get("/workouts", (req, res) => {
+router.get("/stats", (req, res) => {
     Workouts.find({})
     .then(dbWorkouts => {
         res.json(dbWorkouts);
@@ -15,14 +15,13 @@ router.post("/exercise", ({ body }, res) => {
     Workouts.create(body)
     .then(dbWorkouts => {
         res.json(dbWorkouts);
-        res.render("/exercise");
     })
     .catch(error => {
         res.status(400).json(error);
     });
 });
 
-router.get("/stats", (req, res) => {
+router.get("/exercise", (req, res) => {
     Workouts.find({})
     .then(dbWorkouts => {
         res.json(dbWorkouts);
